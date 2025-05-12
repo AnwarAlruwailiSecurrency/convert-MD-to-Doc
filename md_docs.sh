b8992665a526e6ed6bace393c781ac08c201e0f5
AnwarAlruwailiSecurrency <anwar.alruwaili@securrency.com>
2025-05-12 11:37:49 -0400

    chore(dev) github base url for each file

--- md_docs.sh
+++ md_docs.sh
@@ -9,27 +9,34 @@ then
     exit
 fi
 
+# first argument is the input directory
 INPUT_DIR="$1"
+# second argument is the github base url for each file
+GITHUB_URL="$2"
 
 
-if [ -z "$INPUT_DIR" ]; then
-    echo "Usage: $0 <input_directory>"
+
+
+if [ -z "$INPUT_DIR" ] || [ -z "$GITHUB_URL"]; then
+    echo "Usage: $0 <input_directory> <github_base_url>"
     exit 1
 fi
 
 cd "$INPUT_DIR" || exit
 mkdir -p word_docs
+
 for file in *.md; do
     # Check if there are no markdown files
     echo "Processing $file..."
-    # Check if the file is a markdown file
-    # if [[ "$file" == *.md ]]; then
-        # Remove the .md extension and add .docx
-        output_file="${file%.md}.docx"    
-        # Convert the markdown file to docx using pandoc
-        pandoc "$file" -s -o  "word_docs/$output_file"
-        
-        echo "Converted $file to word_docs/$output_file"
-    # fi
+  
+    output_file="${file%.md}.docx"
+    # temp=file="${file%.md}_temp.md"
+
+    # sed "s|../../contracts/\(.*)|$GITHUB_URL/\1|g" "$file" > "$temp"
+    # Convert the markdown file to docx using pandoc
+    pandoc "$file" -s -o  "word_docs/$output_file"
+    
+    echo "Converted $file to word_docs/$output_file"
+    
 done
 echo "All markdown files have been converted to Word documents."
